a1addc7e57a815afe3559bb9d7719f93f9a6896a
PavM9 <p.i.myasnikov@mail.ru>
2022-11-19 13:27:50 +0300

    fix: исправить регулярное выражение

--- backend/utils/utils.js
+++ backend/utils/utils.js
@@ -1,5 +1,5 @@
 function validateUrl(url) {
-  const regex = /https?:\/\/(www)?[0-9a-z\-._~:/?#[\]@!$&'()*+,;=]+(?:\.[a-zA-Z0-9-]+)$/i;
+  const regex = /^https?:\/\/(www)?[0-9a-z\-._~:/?#[\]@!$&'()*+,;=]+(?:\.[a-zA-Z0-9-]+)*$/i;
   if (regex.test(url)) {
     return url;
   }
